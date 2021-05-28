5953eea41c0cdbc7e4057ea545120a20221b5d2c
Kelley Worthington <kelleymarne@gmail.com>
2021-05-27 18:10:16 -0600

    save the task descriptors to local storage

--- main.js
+++ main.js
@@ -3,3 +3,19 @@
     var currentDay = moment().format('MMMM Do YYYY');
     $('#currentDay').html(currentDay);
     console.log(currentDay);
+
+// save button click listener
+$(document).ready(function() {
+    $(".saveBtn").on('click', function() {
+        // Get the values in the textarea descriptions
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        console.log(this);
+
+        // save text in local storage
+        localStorage.setItem(time, text);
+    });
+
+});
+
+
