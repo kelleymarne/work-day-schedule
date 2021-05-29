6b722f7ed142c87c7ded16f0ade322692b0f00d3
Kelley Worthington <kelleymarne@gmail.com>
2021-05-28 22:26:44 -0600

    updated readme

---
+++ README.md
@@ -0,0 +1,47 @@
+# Challenge 4- Timed Test Application 
+> Work Day Planner!
+---
+### Table of Contents
+- [Description](#description)
+- [Introduction](#introduction)
+- [Challenges](#challenges)
+- [Victories](#victories)
+- [Resources](#resources)
+
+---
+
+## Description
+
+Challenge #5 - Work Day Planner
+
+## Introduction 
+This week we were challenged to add functionality to a work day agenda. We were challenged to add the date to the top of the page for it to be current each day. We were instructed to make the fields editable and then save our text when clicking the save button. We also needed our text to persist after 
+
+
+---
+
+## Challenges
+
+Overall, after doing the javascript quiz last week, this challenge was worlds easier and actually pretty enjoyable. I absolutley love when you put code and you see your page come to life. 
+
+---
+## Victories
+
+I struggled to get the localStorage set up and finally got that worked out, first to save the value and key in the local storage and then next to get it to populate on the page. 
+
+![screenshot](./screenshots/localStorageSuccess.jpg)
+![screenshot](./screenshots/localStorageRetrieval.jpg)
+
+---
+
+
+## Resources 
+
+<a href="https://github.com/kelleymarne/work-day-schedule">GitHub Repo</a>
+
+<a href="https://www.youtube.com/watch?v=eVGEea7adDM"> YouTube</a>
+
+<a href="https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwiLjJ7fosLvAhWXW80KHawRD_oQPAgI">Google</a>
+
+<a href="https://www.w3schools.com/charsets/ref_html_ascii.asp">w3 Schools</a>
+
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 
 // TODAYS DATE AT TOP OF PAGE//
-    var currentDay = moment().format('MMMM Do YYYY');
-    $('#currentDay').html(currentDay);
-    console.log(currentDay);
+var currentDay = moment().format('MMMM Do YYYY');
+$('#currentDay').html(currentDay);
+console.log(currentDay);
+
 
 // save button click listener
-$(document).ready(function() {
-    $(".saveBtn").on('click', function() {
+$(document).ready(function () {
+    $(".saveBtn").on('click', function () {
         // Get the values in the textarea descriptions
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
@@ -14,37 +15,52 @@ $(document).ready(function() {
 
         // save text in local storage
         localStorage.setItem(time, text);
+
     });
 
     // Time Tracker Function
     function timeTracker() {
         var timeNow = moment().hour();
 
-    // time block loops
-    $('.time-block').each(function () {
-        var timeBlock = parseInt($(this).attr('id').split('hour')[1]);
+        // time block loops
+        $('.time-block').each(function () {
+            var timeBlock = parseInt($(this).attr('id').split('hour')[1]);
 
-        // Check past, present, future and change classes for color indicator
-        if(timeBlock < timeNow) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        } 
-        
-        else if (timeBlock === timeNow) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        }
-        
-        else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
+            // Check past, present, future and change classes for color indicator
+            if (timeBlock < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
 
-        }
-    })
-}
+            else if (timeBlock === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+
+    // retrieve from local storage
+    $('#hour8 .description').val(localStorage.getItem('hour8'));
+    $('#hour9 .description').val(localStorage.getItem('hour9'));
+    $('#hour10 .description').val(localStorage.getItem('hour10'));
+    $('#hour11 .description').val(localStorage.getItem('hour11'));
+    $('#hour12 .description').val(localStorage.getItem('hour12'));
+    $('#hour13 .description').val(localStorage.getItem('hour13'));
+    $('#hour14 .description').val(localStorage.getItem('hour14'));
+    $('#hour15 .description').val(localStorage.getItem('hour15'));
+    $('#hour16 .description').val(localStorage.getItem('hour16'));
+    $('#hour17 .description').val(localStorage.getItem('hour17'));
+
 
     timeTracker();
 });
